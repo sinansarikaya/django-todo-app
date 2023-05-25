1ef3bc9e9f997b8730cdca4ddbfe9358714cfa4a
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-25 12:05:53 +0200

    UPDATE: Admin page

--- todos/admin.py
+++ todos/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import TodosModel, ContactModel
 
-admin.site.register(TodosModel)
-admin.site.register(ContactModel)
+@admin.register(TodosModel)
+class TodosAdmin(admin.ModelAdmin):
+    list_display = ["title", "is_completed", "created_at", "updated_at"]
+    list_display_links = ["title"]
+    list_editable = ["is_completed"]
+    search_fields = ["title", "content"]
+    list_filter = ["is_completed", "created_at", "updated_at"]
+    list_per_page = 25
+    date_hierarchy = "created_at"
+    ordering = ["-created_at", "title"]
+    readonly_fields = ["created_at"]
+    class Meta:
+        model: TodosModel     
+        verbose_name = _("Todo")
+        verbose_name_plural = _("Todos") 
+  
+@admin.register(ContactModel)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["subject", "email", "created_at", "updated_at"]
+    list_display_links = ["subject"]
+    search_fields = ["subject","message"]
+    list_filter = ["created_at"]
+    date_hierarchy = "created_at"
+    ordering = ["-created_at", "subject"]
+    readonly_fields = ["created_at"]
+
+    class Meta:
+        model: ContactModel     
+        verbose_name = _("Contact")
+        verbose_name_plural = _("Contacts")   
+
