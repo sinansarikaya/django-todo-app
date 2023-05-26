c4c29c182537ef45d0eea6562d4fcde70e94c6d4
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-26 21:48:54 +0200

    CHANGE: Static location

--- core/settings.py
+++ core/settings.py
@@ -132,10 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = 'login'  # Giriş sayfasının URL adresi
 LOGIN_REDIRECT_URL = '/' # Başarılı giriş işleminden sonra yönlendirilecek sayfa
