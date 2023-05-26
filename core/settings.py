65bbe887f46a828970eae1c76d5662da1ad15bc5
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-26 22:03:04 +0200

    UPDATE: Static dir

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = 'login'  # Giriş sayfasının URL adresi
