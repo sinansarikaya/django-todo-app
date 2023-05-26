6b51e29af0241b9e75b446a59cef0ad3cb73cab3
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-26 21:39:57 +0200

    UPDATE: Vercel settings

--- core/settings.py
+++ core/settings.py
@@ -131,16 +131,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    "/var/www/static/",
 ]
-
-# MEDIA FILES
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'  # Giriş sayfasının URL adresi
 LOGIN_REDIRECT_URL = '/' # Başarılı giriş işleminden sonra yönlendirilecek sayfa
