1e30ff766f13bb02d849cbd9c1c935ad214f6352
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-26 19:18:42 +0200

    ADD: Postgresql

--- core/settings.py
+++ core/settings.py
@@ -82,9 +82,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": { 
+        "ENGINE": "django.db.backends.postgresql_psycopg2", 
+        "NAME": config("SQL_DATABASE"), 
+        "USER": config("SQL_USER"), 
+        "PASSWORD": config("SQL_PASSWORD"), 
+        "HOST": config("SQL_HOST"), 
+        "PORT": config("SQL_PORT"), 
+        "ATOMIC_REQUESTS": True,
     }
 }
 
