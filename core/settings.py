00ad1aa60409b35e4f8f8859ec5fe0d0559059c0
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-05-27 08:07:35 +0200

    CREATE: Static root

--- core/settings.py
+++ core/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = 'login'  # Giriş sayfasının URL adresi
 LOGIN_REDIRECT_URL = '/' # Başarılı giriş işleminden sonra yönlendirilecek sayfa
